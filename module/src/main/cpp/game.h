e1e9f7806dd3f9d3a5bf1b9e1c827f4f74c0fd04
январский <dolboebovaruk@gmail.com>
2026-04-27 22:13:41 +0200

    Update GamePackageName to new package identifier

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "com.skullcapstudios.bpm"
+#define GamePackageName "com.axlebolt.standoff2"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
